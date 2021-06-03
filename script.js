1a1b77c261abc9901edf228617e778000117a3c0
Tasneemalabsi <>
2021-06-03 12:01:28 +0000

    adding script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+var firstname = "Tasneem";
+var lastname= " Al-Absi";
+var name= firstname + lastname
+console.log(name)
+document.write("<p> designed-by: </p>"+ name)
+
+alert("Welcome to Wadi-rum page")
+
+if(confirm("are you interested in joining our trip?"))
+{alert("thank you,we will send you the info. soon")}
+else{alert("Welcome anyway")}
+
+if(prompt("are you from Amman?")== "yes"){
+alert("your trip is at 10 am")}
+else{alert("your trip is at 8 am")}
+
+
+ 
+
+
+
