8b8c3790d850b6d5c958d1fddec4fd05d71b11cd
Tasneemalabsi <>
2021-06-06 12:59:29 +0000

    edit 4

--- script.js
+++ script.js
@@ -22,12 +22,15 @@ places ()
 
 function rating () {
   for (var i = 1; i<=rate; i++){
-  document.write('<img src = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Star_full.svg/1200px-Star_full.svg.png" width="50" height="50"/>')
+  document.write('<img src = "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c0/Location_dot_black.svg/1024px-Location_dot_black.svg.png" width="50" height="50"/>')
 }
 return i;
 
 }
  rating ()
+
+ // I changed the picture in the function rating so that you won't get lost because the function will double the number of pictures that are already in the original for loop
+
  
 
 
